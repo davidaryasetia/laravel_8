a7381d82c81a34855c6343396ea451ec68631594
David Aryasetia <david.aryasetia@gmai.com>
2024-01-04 11:55:20 +0700

    Post Model

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,11 +5,15 @@
 @section('container')
 
 <article>
-    <h2> {{$post["title"]}} </h2>
-    <h5> {{$post["author"]}} </h5>
-    <p>  {{$post["body"]}} </p>
+    <h2> {{$post->title}} </h2>
+    <h5> {{$post->author}} </h5>
+    {!! $post->body !!}
 </article>
 
 <a href="/posts">Back To Blog</a>
 
 @endsection
+
+
+
+
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,11 +5,19 @@
     @foreach ($posts as $post)
     <article class="mb-5">
         <h2>
-           <a href="/posts/{{ $post["slug"]}}"> {{$post["title"]}} </a>
+           <a href="/posts/{{ $post->slug }}"> {{$post->title}} </a>
         </h2>
-        <h5>By:  {{$post["author"]}} </h5>
-        <p> {{$post["body"]}} </p>
+        <h5>By:  {{$post->author}} </h5>
+        <p> {{$post->excerpt}} </p>
     </article>
         @endforeach
 
     @endsection
+
+{{--
+$post::create([
+    'title' => 'judul ketiga3',
+    'slug' => 'judul-ketiga',
+    'excerpt' => 'Lorem ipsum pertama dolor sit amet consecte',
+    'body' => '<p>Reiciendis nemo optio maiores aut obcaecati saepe unde earum! Fugiat possimus amet vero voluptate ullam, eveniet quas odit iste recusandae unde providentsaepe distinctio iure laborum placeat at voluptatem deserunt tenetur adipisci.</p><p> Molestias, distinctio dolore. Mollitia deleniti, nemo suscipit eaque voluptatibus omnis laudantium, pariatur eveniet, totam incidunt vel corporis debitis quod perspiciatis asperiores culpa aliquam. Dolore temporibus quam laudantium debitis doloremque cum rerum natus laborum animi? Maxime nihil, aperiam ullam minus, velit expedita consequatur </p><p>impedit beatae, numquam reprehenderit laborumvoluptates itaque dolore nemo aspernatur molestias voluptas. Quo ipsa dolores, omnis voluptatumneque delectus id molestiae expedita quaerat perferendis maiores, debitis optio sapiente sint illum enim quis vel laboriosam tenetur harum quia ex nihil eius. Sit sequi aperiam nemo enim necessitatibus laborum laudantium?</p>'
+]) --}}
