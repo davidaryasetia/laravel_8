7052ad27d569c6b75050d84ea005c51a979ac1ff
David Aryasetia <david.aryasetia@gmail.com>
2024-01-08 11:25:48 +0700

    Add search & pagination

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             @foreach($categories as $category)
             <div class="col-md-4">
-                <a href="/categories/{{$category->slug}}">
+                <a href="/posts?category={{$category->slug}}">
                 <div class="card text-bg-dark">
                     <img src="https://source.unsplash.com/random/640×320?{{$category->name}}" class="card-img-top" alt="{{$category->name}}" class="card-img" alt="{{$category->name}}">
                     <div class="card-img-overlay d-flex align-items-center p-0">
