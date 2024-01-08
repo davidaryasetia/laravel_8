ab15e38ca35070f9d6819466062b63d932a5fefb
David Aryasetia <david.aryasetia@gmail.com>
2024-01-08 16:47:08 +0700

    final register user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -11,5 +13,24 @@ class RegisterController extends Controller
         return view('register.index', [
             'title' => 'Register'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        // $validatedData['password'] = bcrypt($validatedData('password'));
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+        // $request->session()->flash('success', 'Registration Successfull!! please login');
+
+        return redirect('/login')->with('success', 'Registration Successfully!! Please Login');
     }
 }
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -4,23 +4,45 @@
 <div class="row justify-content-center">
     <div class="col-lg-5">
         <main class="form-registration w-100 m-auto">
-            <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
-            <form>
+            <h1 class="h3 mb-3 fw-normal text-center">Registration F orm</h1>
+            <form action="/register" method="post">
+                @csrf
               <div class="form-floating">
-                <input type="text" name="name" class="form-control" rounded-top id="name" placeholder="Name">
+                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror required" rounded-top id="name" placeholder="Name" value="{{old('name')}}">
                 <label for="name">Name</label>
+                @error('name')
+                <div class="invalid-feedback">
+                    {{$message}}
+                  </div>
+                @enderror
               </div>
+
               <div class="form-floating">
-                <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+                <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" id="username " placeholder="Username" value="{{old('username')}}" required>
                 <label for="username">UserName</label>
+                @error('username')
+                <div class="invalid-feedback">
+                  {{$message}}
+                </div>
+                @enderror
               </div>
+
               <div class="form-floating">
-                <input type="email" name="email" class="form-control" id="email" placeholder="email">
-                <label for="email">email</label>
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror " id="email" placeholder="email" value="{{old('email')}}" required>
+                <label fo r="email">email</label>
+                @error('email')
+                <div class="invalid-feedback">
+                  {{$message}}
+                </div>
+                @enderror
               </div>
+
               <div class="form-floating" rounded-bottom>
-                <input type="password" name="password" class="form-control" id="password" placeholder="password">
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror " id="password" placeholder="password" required>
                 <label for="password">password</label>
+                @error('password')
+                {{$message}}
+                @enderror
               </div>
 
               <button class="btn btn-primary w-100 py-2 mt-3" type="submit">Register</button>
