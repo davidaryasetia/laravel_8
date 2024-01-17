764165131269347bd63863e7f52c41e203c26bd0
David Aryasetia <david.aryasetia@gmail.com>
2024-01-17 16:38:18 +0700

    Add Authorization User

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -68,9 +68,10 @@ class DashboardPostController extends Controller
     /**
      * Display the specified resource.
      *
+     *
      * @param  \App\Models\Post  $post
      * @return \Illuminate\Http\Response
-     */
+    */
     public function show(Post $post)
     {
         return view('dashboard.posts.show', [
