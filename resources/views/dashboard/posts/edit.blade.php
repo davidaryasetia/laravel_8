861c371d8501bfd38dff8020b4fb9bd1adfae276
David Aryasetia <david.aryasetia@gmail.com>
2024-01-17 11:04:08 +0700

    add update & delete image

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -7,7 +7,7 @@
 </div>
 
 <div class="col-lg-8">
-<form method="post" action="/dashboard/posts/{{$post->slug}}" class="mb-5">
+<form method="post" action="/dashboard/posts/{{$post->slug}}" class="mb-5" enctype="multipart/form-data">
     @method('put')
     @csrf
 <div class="mb-3">
@@ -44,6 +44,23 @@
     </div>
 
     <div class="mb-3">
+        <label for="image" class="form-label">Post Image</label>
+        <input type="hidden" name="oldImage" value="{{$post->image}}">
+        @if($post->image)
+        <img src="{{asset('storage/'. $post->image)}}" class="img-preview img-fluid mb-3 col-sm-5 d-block" alt="">
+        @else
+        <img class="img-preview img-fluid mb-3 col-sm-5" src="" alt="">
+        @endif
+        <input class="form-control @error('image') is-invalid @enderror"  type="file" id="image" name="image" onchange="previewImage()">
+        @error('image')
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+        @enderror
+    </div>
+
+
+    <div class="mb-3">
       <label for="body" class="form-label">Body</label>
       <input id="body" type="hidden" name="body" value="{{old('body', $post->body)}}">
       <trix-editor input="body"></trix-editor>
@@ -71,6 +88,20 @@
     document.addEventListener('trix-file-accept', function(e){
         e.preventDefault();
     })
+
+    function previewImage(){
+        const image = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+
+        imgPreview.style.display = 'block';
+
+        const oFReader = new FileReader();
+        oFReader.readAsDataURL(image.files[0]);
+
+        oFReader.onload = function(oFREvent){
+            imgPreview.src = oFREvent.target.result;
+        }
+    }
 </script>
 @endsection
 
